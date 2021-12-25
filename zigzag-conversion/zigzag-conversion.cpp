0e32c085a31b81fd7b53f005d114a49df310ae6e
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2021-12-25 14:40:18 -0500

    Time: 12 ms (58.25%), Space: 7.8 MB (99.13%) - LeetHub

--- zigzag-conversion/zigzag-conversion.cpp
+++ zigzag-conversion/zigzag-conversion.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<vector<char>> ans(numRows);
-        int temp = 0;
+        
         if(numRows == 1)
             return s;
-        bool flipped = false;
-        for(int i = 0; i < s.length(); i++) {
-            if(temp == numRows)
-            {
-                temp = numRows - 2;
-                flipped = true;
+        
+        string ans = s;
+        int temp = 0;
+        for(int i = 0; i < numRows; i++) {
+            if(i == 0) {
+                for(int j = 0; j < s.length(); j+=numRows + numRows - 2) {
+                    ans[temp++] = s[j];
+                }
+                continue;
             }
-            if(temp == -1)
-            {
-                temp += 2;
-                flipped = false;
+            else if(i == numRows - 1) {
+                for(int j = numRows - 1; j < s.length(); j+=numRows + numRows - 2) {
+                    ans[temp++] = s[j];
+                }
+                continue;
             }
-            ans[temp].push_back(s[i]);
-            if(flipped)
-                temp--;
-            else
-                temp++;
+            bool flipped = false;
+            for(int j = i; j < s.length(); j++) {
+                if(!flipped) {
+                    ans[temp++] = s[j];
+                    j += 2*(numRows - 1 - i);
+                    j--;
+                }
+                else {
+                    ans[temp++] = s[j];
+                    j += 2*i;
+                    j--;
+                }
+                flipped = !flipped;
+            }
         }
-        string res = "";
-        for(auto x : ans) {
-            for(auto y : x)
-                res += y;
-        }
-        return res;
+        return ans;
     }
 };
