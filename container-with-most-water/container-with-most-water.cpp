3ab3e64d47b234691c4fe54cd13a27b9fb4481d4
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2021-12-26 19:06:43 -0500

    Time: 80 ms (78.39%), Space: 59 MB (36.62%) - LeetHub

--- container-with-most-water/container-with-most-water.cpp
+++ container-with-most-water/container-with-most-water.cpp
@@ -1,52 +1,16 @@
 class Solution {
 public:
-    
-    static bool comp(pair<int,int> a, pair<int,int> b) {
-        if(a.first != b.first)
-            return a.first > b.first;
-        
-        return a.second < b.second; 
-    }
-    
     int maxArea(vector<int>& height) {
-        vector<pair<int,int>> temp;
-        for(int i = 0; i < height.size(); i++)
-            temp.push_back(make_pair(height[i],i));
-        
-        sort(temp.begin(),temp.end(),comp);
-        
-        int rightmost = -1, leftmost = 1e9, ans = -1;
-        for(int i = 0; i < temp.size(); i++) {
-            if(i == 0) {
-                rightmost = max(rightmost,temp[i].second);
-                leftmost = min(leftmost,temp[i].second);
+        int maxi = -1, start = 0, end = height.size() - 1;
+        while(start < end) {
+            maxi = max(maxi, (end - start) * min(height[start],height[end]));
+            if(height[start] < height[end]) {
+                start++;
             }
             else {
-                if(rightmost == leftmost) {
-                    ans = max(ans,temp[i].first * abs(temp[i].second - rightmost));
-                    rightmost = max(rightmost,temp[i].second);
-                    leftmost = min(leftmost,temp[i].second);
-                }
-                else {
-                    if(temp[i].second < leftmost) {
-                        ans = max(ans,temp[i].first * abs(temp[i].second - rightmost));
-                        rightmost = max(rightmost,temp[i].second);
-                        leftmost = min(leftmost,temp[i].second);
-                    }
-                    else if(temp[i].second > rightmost) {
-                        ans = max(ans,temp[i].first * abs(temp[i].second - leftmost));
-                        rightmost = max(rightmost,temp[i].second);
-                        leftmost = min(leftmost,temp[i].second);
-                    }
-                    else {
-                        ans = max(ans,temp[i].first * abs(leftmost - rightmost));
-                        rightmost = max(rightmost,temp[i].second);
-                        leftmost = min(leftmost,temp[i].second);
-                    }
-                }
+                end--;
             }
         }
-        return ans;
-        return 0;
+        return maxi;
     }
 };
