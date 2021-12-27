6e420e4965b70b625be1daa520fa2ce2ef8eaa61
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2021-12-27 15:11:37 -0500

    Time: 112 ms (25.05%), Space: 48.2 MB (78.23%) - LeetHub

---
+++ jump-game/jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        if(nums.size() == 1)
+            return true;
+        int cango = nums[0];
+        for(int i = 1; i < nums.size() and cango > 0; i++) {
+            cango--;
+            cango = max(cango, nums[i]);
+            if(i + cango >= nums.size() - 1)
+                return true;
+            
+            cout<<cango<<" ";
+        }
+        return false;
+    }
+};
