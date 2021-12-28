fd9217987016fd965010436cf824225fb76ce1c9
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2021-12-28 11:16:01 -0500

    Time: 32 ms (84.07%), Space: 36.1 MB (95.58%) - LeetHub

--- max-consecutive-ones-ii/max-consecutive-ones-ii.cpp
+++ max-consecutive-ones-ii/max-consecutive-ones-ii.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        if(nums.size() == 1) {
-            return 1;
-        }
-        vector<int> consecfromleft(nums.size()), consecfromright(nums.size());
-        int count = 0;
+        
+        int numofzeros = 0, lastzeroat = -1;
+        int left = 0, right = 0;
         int maxi = -1;
         for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == 1) {
-                count++;
-                maxi = max(maxi,count);
-            }
-            else {
-                count = 0;
-            }
-            consecfromleft[i] = count;
-        }
-        count = 0;
-        for(int i = nums.size() - 1; i >= 0; i--) {
-            if(nums[i] == 1) {
-                count++;
-            }
-            else {
-                count = 0;
-            }
-            consecfromright[i] = count;
-        }
-        for(int i = 0; i < nums.size(); i++) {
             if(nums[i] == 0) {
-                if(i == 0) {
-                    maxi = max(maxi,1 + consecfromright[i+1]);
-                }
-                else if(i == nums.size() - 1) {
-                    maxi = max(maxi,1 + consecfromleft[i-1]);
+                if(numofzeros == 0) {
+                    lastzeroat = i;
+                    numofzeros++;
+                    right++;
+                    maxi = max(maxi,right - left);
                 }
                 else {
-                    maxi = max(maxi,1 + consecfromleft[i-1] + consecfromright[i+1]);
+                    maxi = max(maxi,right - left);
+                    numofzeros = 1;
+                    left = lastzeroat + 1;
+                    right = i+1;
+                    lastzeroat = i;
+                    maxi = max(maxi,right - left);
                 }
             }
+            else {
+                right++;
+                maxi = max(maxi,right - left);
+            }
+            maxi = max(maxi,right - left);
         }
         return maxi;
+        
     }
 };
