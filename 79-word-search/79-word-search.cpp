9b8b4c825d224f52212b47117017d8233e05042f
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-05 10:26:00 -0500

    Time: 1204 ms (5.02%), Space: 8.6 MB (5.28%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    
+    bool check(vector<vector<char>>& board, int x, int y, 
+               vector<vector<bool>>& taken, string current, string word) {
+        
+        if(word.substr(0,current.length()) != current) {
+            return false;
+        }
+        if(current == word) {
+            return true;
+        }
+        if(x >= board.size() or y >= board[0].size() or x < 0 or y < 0) {
+            return false;
+        }
+        if(current.length() > word.length()) {
+            return false;
+        }
+        if(taken[x][y] == true) {
+            return false;
+        }
+        
+        taken[x][y] = true;
+        
+        bool ans = false;
+        
+        ans = ans or check(board,x+1,y,taken,current + board[x][y],word) or 
+            check(board,x,y+1,taken,current + board[x][y],word) or 
+            check(board,x-1,y,taken,current + board[x][y],word) or 
+            check(board,x,y-1,taken,current + board[x][y],word);
+        
+        taken[x][y] = false;
+        
+        return ans;
+               
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        bool found = false;
+        
+        if(word.length() > (int)board.size() * (int)board[0].size())
+            return found;
+        
+        map<char,int> m;
+        for(auto x : board) {
+            for(auto y : x) {
+                m[y]++;
+            }
+        }
+        for(auto x : word) {
+            m[x]--;
+            if(m[x] < 0) {
+                return found;
+            }
+        }
+        
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0; j < board[i].size(); j++) {
+                vector<vector<bool>> taken(board.size(),vector<bool>(board[0].size()));
+                found = found or check(board,i,j,taken,"",word);
+            }
+        }
+        
+        return found;
+    }
+};
