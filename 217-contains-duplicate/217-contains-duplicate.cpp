1a653553c21ae61bb8873290b90a682013407f12
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-04 21:04:12 -0500

    Time: 158 ms (6.38%), Space: 52.2 MB (10.71%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> m;
+        map<int,int> m;
         for(auto x : nums) {
             if(m[x] == 1) {
                 return true;
