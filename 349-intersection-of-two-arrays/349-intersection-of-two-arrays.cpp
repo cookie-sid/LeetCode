356272700cc2ec31638b77590e4ca392f49c3abd
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-05 20:44:39 -0500

    Time: 13 ms (12.51%), Space: 9.9 MB (90.95%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> m;
-        for(auto x : nums1) {
-            m[x] = 1;
-        }
-        set<int> ans;
-        for(auto x : nums2) {
-            if(m[x] != 0) {
-                ans.insert(x);
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        vector<int> ans;
+        
+        int start_1 = 0, start_2 = 0;
+        
+        while(start_1 < nums1.size() and start_2 < nums2.size()) {
+            while(start_1 < nums1.size() and nums1[start_1] < nums2[start_2]) {
+                start_1++;
+            }
+            if(start_1 == nums1.size()) {
+                break;
+            }
+            if(nums1[start_1] == nums2[start_2]) {
+                ans.push_back(nums1[start_1]);
+                while(start_1 < nums1.size() and start_2 < nums2.size() and nums1[start_1] == nums2[start_2]) {
+                    if(ans.back() != nums1[start_1]) {
+                        ans.push_back(nums1[start_1]);
+                    }
+                    start_1++;
+                    start_2++;
+                }
+            }
+            else {
+                while(start_2 < nums2.size() and nums2[start_2] < nums1[start_1]) {
+                    start_2++;
+                }
+                if(start_2 == nums2.size()) {
+                    break;
+                }
+                if(nums1[start_1] == nums2[start_2]) {
+                    ans.push_back(nums1[start_1]);
+                    while(start_1 < nums1.size() and start_2 < nums2.size() and nums1[start_1] == nums2[start_2]) {
+                        if(ans.back() != nums1[start_1]) {
+                            ans.push_back(nums1[start_1]);
+                        }
+                        start_1++;
+                        start_2++;
+                    }
+                }
             }
         }
-        vector<int> res;
-        for(auto x : ans) {
-            res.push_back(x);
-        }
-        return res;
+        
+        return ans;
+        
     }
 };
