9064e559ff7fc5ed9a60d93da07806dd71012894
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-05 20:47:40 -0500

    Time: 3 ms, Memory: 10 MB - LeetHub

---
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        //O(n) time and O(1) space, given that the arrays are already sorted
+        
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        vector<int> ans;
+        
+        int start_1 = 0, start_2 = 0;
+        
+        while(start_1 < nums1.size() and start_2 < nums2.size()) {
+            while(start_1 < nums1.size() and nums1[start_1] < nums2[start_2]) {
+                start_1++;
+            }
+            if(start_1 == nums1.size()) {
+                break;
+            }
+            if(nums1[start_1] == nums2[start_2]) {
+                
+                while(start_1 < nums1.size() and start_2 < nums2.size() and nums1[start_1] == nums2[start_2]) {
+                    ans.push_back(nums1[start_1]);
+                    start_1++;
+                    start_2++;
+                }
+            }
+            else {
+                while(start_2 < nums2.size() and nums2[start_2] < nums1[start_1]) {
+                    start_2++;
+                }
+                if(start_2 == nums2.size()) {
+                    break;
+                }
+                if(nums1[start_1] == nums2[start_2]) {
+                    
+                    while(start_1 < nums1.size() and start_2 < nums2.size() and nums1[start_1] == nums2[start_2]) {
+                        ans.push_back(nums1[start_1]);
+                        start_1++;
+                        start_2++;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
