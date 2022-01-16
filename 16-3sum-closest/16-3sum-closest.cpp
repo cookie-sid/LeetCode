1f4fb63c169372393fb53e9840032b9b9d23d297
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-15 19:54:05 -0500

    Time: 24 ms (25.23%), Space: 9.8 MB (63.45%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -4,24 +4,22 @@ public:
         
         sort(nums.begin(),nums.end());
         
-        int ans = nums[0] + nums[1] + nums[2];
+        int ans = -1e9;
         for(int i = 0; i < nums.size() - 2; i++) {
-            for(int j = i  + 1; j < nums.size() - 1; j++) {
-               
-                int required = target - nums[i] - nums[j];
-                int upper = upper_bound(nums.begin() + j + 1,nums.end(),required) - nums.begin();
-                if(upper == nums.size()) {
-                    if(upper - 1 > j  and abs(target - ans) > abs(target - (nums[i] + nums[j] + nums[upper - 1]))) {
-                        ans = nums[i] + nums[j] + nums[upper - 1];
+            int required = target - nums[i];
+            int start = i + 1, end = nums.size() - 1;
+            while(start < end) {
+                if(nums[start] + nums[end] < required) {
+                    if(abs(target - ans) > abs(target - (nums[i] + nums[start] + nums[end]))) {
+                        ans = nums[i] + nums[start] + nums[end];
                     }
+                    start++;
                 }
                 else {
-                    if(upper - 1 > j and abs(target - ans) > abs(target - (nums[i] + nums[j] + nums[upper - 1]))) {
-                        ans = nums[i] + nums[j] + nums[upper - 1];
+                    if(abs(target - ans) > abs(target - (nums[i] + nums[start] + nums[end]))) {
+                        ans = nums[i] + nums[start] + nums[end];
                     }
-                    if(upper > j and abs(target - ans) > abs(target - (nums[i] + nums[j] + nums[upper]))) {
-                        ans = nums[i] + nums[j] + nums[upper];
-                    }
+                    end--;
                 }
             }
         }
