113c790a0554389403ca207fbc9fe98debaed192
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-20 15:29:33 -0500

    Time: 149 ms (6.79%), Space: 19.6 MB (9.40%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -17,12 +17,12 @@ public:
             }
         }
         
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                cout<<grid[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < m; j++) {
+        //         cout<<grid[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         
         int maxi = -1;
         for(int i = 0; i < m; i++) {
