6d6dc96aec0c23024be895d76419178e7737d864
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-20 22:54:37 -0500

    Time: 127 ms (44.93%), Space: 20.1 MB (66.42%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -8,20 +8,30 @@ public:
             return {};
         }
         sort(nums.begin(),nums.end());
-        map<int,int> m;
-        for(int i = 0; i < nums.size(); i++) {
-            m[nums[i]] = i + 1;
-        }
         vector<vector<int>> ans;
-        for(int i = 0; i < nums.size() - 1; i++) {
-            for(int j = i + 1; j < nums.size(); j++) {
-                if(m[-nums[i]-nums[j]] > j + 1) {
-                    ans.push_back({nums[i],nums[j],-nums[i]-nums[j]});
+        for(int i = 0; i < nums.size() - 2; i++) {
+            
+            int l = i + 1, r = nums.size() - 1;
+            while(l < r) {
+                if(nums[l] + nums[r] < -nums[i]) {
+                    l++;
                 }
-                while(j < nums.size() - 1 and nums[j] == nums[j+1]) {
-                    j++;
+                else if(nums[l] + nums[r] > -nums[i]) {
+                    r--;
+                }
+                else {
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    while(l + 1 < r and nums[l + 1] == nums[l]) {
+                        l++;
+                    }
+                    while(r - 1 > l and nums[r - 1] == nums[r]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
                 }
             }
+            
             while(i < nums.size() - 2 and nums[i] == nums[i+1]) {
                 i++;
             }
