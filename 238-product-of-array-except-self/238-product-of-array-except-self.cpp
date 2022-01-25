3a5fa29dfdcb8deb593d4b41607283bac21f6dad
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-25 08:14:34 -0500

    Time: 36 ms (32.90%), Space: 23.8 MB (92.60%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,43 +1,14 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
-        int zeros = 0;
-        int prod = 1, prod_without_zeros = 1;
-        for(auto x : nums) {
-            if(x == 0) {
-                zeros++;
-                prod *= x;
-                continue;
-            }
-            prod *= x;
-            prod_without_zeros *= x;
+        vector<int> ans(nums.size(),1);
+        int from_start = 1, from_end = 1;
+        for(int i = 0; i < ans.size(); i++) {
+            ans[i] *= from_start;
+            from_start *= nums[i];
+            ans[nums.size() - 1 - i] *= from_end;
+            from_end *= nums[nums.size() - i - 1];
         }
-        
-        if(zeros == 1) {
-            for(int i = 0; i < nums.size(); i++) {
-                if(nums[i] == 0) {
-                    nums[i] = prod_without_zeros;
-                }
-                else {
-                    nums[i] = 0;
-                }
-            }
-            return nums;
-        }
-        
-        if(zeros > 1) {
-            for(int i = 0; i < nums.size(); i++) {
-                nums[i] = 0;
-            }
-            return nums;
-        }
-        
-        for(int i = 0; i < nums.size(); i++) {
-            nums[i] = prod/nums[i];
-        }
-        
-        return nums;
-        
+        return ans;
     }
 };
