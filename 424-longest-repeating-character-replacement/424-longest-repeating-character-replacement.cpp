3f0e7a9385e38b85ea249a86f1ac4c96a7ab7fb3
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-31 14:19:22 -0500

    Time: 12 ms (66.99%), Space: 7 MB (85.44%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     
-    bool check(map<char,int> &m, int k, int total) {
-        for(auto x : m) {
-            if(total - x.second <= k) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
     int characterReplacement(string s, int k) {
         
-        map<char,int> m;
-        int start = 0, ans = 0, total = 0;
+        vector<int> count(26,0);
+        int start = 0, ans = 0, total = 0, maxCountInWindow = 0;
         for(int i = 0; i < s.length(); i++) {
             if(i == 0) {
-                m[s[i]]++;
-                ans = max(ans,i - start + 1);
+                count[s[i] - 'A']++;
+                maxCountInWindow = max(maxCountInWindow, count[s[i] - 'A']);
                 total++;
+                ans = max(ans,i - start + 1);
                 continue;
             }
-            m[s[i]]++;
+            count[s[i] - 'A']++;
+            maxCountInWindow = max(maxCountInWindow, count[s[i] - 'A']);
             total++;
-            while(start < i and !check(m,k,total)) {
-                m[s[start]]--;
+            if(total - maxCountInWindow > k) {
+                count[s[start] - 'A']--;
                 start++;
                 total--;
             }
