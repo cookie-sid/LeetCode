090a397d9eb36c0fb6ff79f0858ffb2b5a211267
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-01-31 16:20:44 -0500

    Time: 56 ms (76.38%), Space: 55.5 MB (13.80%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        queue<int> q;
         int start = 0, ans = 0;
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] == 0) {
-                q.push(i+1);
+                k--;
             }
-            else {
-                ans = max(i - start + 1,ans);
+            if(k < 0) {
+                k += 1 - nums[start];
+                start++;
             }
-            
-            if(q.size() > k ) {
-                start = q.front();
-                q.pop();
+            if(k >= 0) {
+                ans = max(ans,i - start + 1);
             }
-            
-            ans = max(i - start + 1,ans);
-            
+
         }
         return ans;
     }
