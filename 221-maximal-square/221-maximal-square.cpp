0514988ab01657006f695e007081e0adfc6219c9
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-05 09:19:51 -0500

    Time: 107 ms (19.54%), Space: 19.5 MB (14.17%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
+        
         int n = matrix.size(), m = matrix[0].size();
-        vector<vector<int>> grid(n,vector<int> (m,0));
+        vector<vector<int>> grid (n, vector<int> (m,0));
+        
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 grid[i][j] = matrix[i][j] - '0';
-            }
-        }
-        for(int i = 0; i < n; i++) {
-            for(int j = 1; j < m; j++) {
-                if(grid[i][j] == 0) {
-                    continue;
+                if(j > 0 and grid[i][j] != 0) {
+                    grid[i][j] += grid[i][j-1];
                 }
-                grid[i][j] += grid[i][j-1];
             }
         }
         
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = 0; j < m; j++) {
-        //         cout<<grid[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        
-        int maxi = -1;
-        for(int i = 0; i < m; i++) {
+        int ans = -1;
+        for(int j = 0; j < m; j++) {
             stack<int> s;
             s.push(-1);
-            for(int j = 0; j < n; j++) {
-                while(s.top() != -1 and grid[s.top()][i] >= grid[j][i]) {
-                    int up = s.top();
+            for(int i = 0; i < n; i++) {
+                while(s.top() != -1 and grid[s.top()][j] >= grid[i][j]) {
+                    int popped = s.top();
                     s.pop();
-                    if(j - s.top() - 1 >= grid[up][i]) {
-                        maxi = max(maxi,grid[up][i]*grid[up][i]);
-                    }
+                    int mini = min(i - (s.top() + 1),grid[popped][j]);
+                    ans = max(ans,mini*mini);
                 }
-                if(j - s.top() >= grid[j][i]) {
-                    maxi = max(maxi,grid[j][i] * grid[j][i]);
-                }
-                s.push(j);
+                s.push(i);
             }
-            int last = n - 1;
             while(s.top() != -1) {
-                int up = s.top();
+                int popped = s.top();
                 s.pop();
-                if(last - s.top() >= grid[up][i]) {
-                    maxi = max(maxi,grid[up][i]*grid[up][i]);
-                }
-                last = up;
+                int mini = min(n - (s.top() + 1), grid[popped][j]);
+                ans = max(ans,mini * mini);
             }
         }
-        return maxi;
+        
+        return ans;
     }
 };
