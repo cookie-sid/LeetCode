dd80f128e7e8b7a8e81ff367dea040a9e4eac815
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-10 19:22:42 -0500

    Time: 89 ms (15.72%), Space: 29.9 MB (8.41%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     
-    bool cmp(vector<int> &temp1, vector<int> &temp2) {
-        for(int i = 0; i < temp1.size(); i++) {
-            if(temp1[i] != temp2[i]) {
+    bool check(vector<int> one, vector<int> two) {
+        for(int i = 0; i < one.size(); i++) {
+            if(one[i] != two[i])
                 return false;
-            }
         }
         return true;
     }
     
     bool checkInclusion(string s1, string s2) {
-        if(s1.length() > s2.length()) return false;
-        vector<int> freqOne(26,0), freqCalc(26,0);
+        if(s1.length() > s2.length())
+            return false;
+        vector<int> pattern(26,0), current(26,0);
         for(auto x : s1) {
-            freqOne[x - 'a']++;
+            pattern[x - 'a']++;
         }
-        for(int i = 0; i < s1.length(); i++) {
-            freqCalc[s2[i] - 'a']++;
-        }
-        if(cmp(freqOne,freqCalc)) {
-            return true;
-        }
-        for(int i = s1.length(); i < s2.length(); i++) {
-            freqCalc[s2[i] - 'a']++;
-            freqCalc[s2[i-s1.length()] - 'a']--;
-            if(cmp(freqOne,freqCalc)) {
-                return true;
+        for(int i = 0; i < s2.length() - s1.length() + 1; i++) {
+            if(i == 0) {
+                for(int j = 0; j < s1.length(); j++) {
+                    current[s2[j] - 'a']++;
+                }
+                if(check(pattern,current))
+                    return true;
+            }
+            else {
+                current[s2[i + s1.length() - 1] - 'a']++;
+                current[s2[i-1] - 'a']--;
+                if(check(pattern,current))
+                    return true;
             }
         }
+        
         return false;
     }
 };
