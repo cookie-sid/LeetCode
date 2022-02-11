6fce6735e49d889c09fa752c1137c09333572323
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-10 19:30:28 -0500

    Time: 11 ms (55.39%), Space: 7.3 MB (71.57%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     
-    bool check(vector<int> one) {
-        for(int i = 0; i < one.size(); i++) {
-            if(one[i] != 0)
+    bool cmp(vector<int> &temp1, vector<int> &temp2) {
+        for(int i = 0; i < temp1.size(); i++) {
+            if(temp1[i] != temp2[i]) {
                 return false;
+            }
         }
         return true;
     }
     
     bool checkInclusion(string s1, string s2) {
-        if(s1.length() > s2.length())
-            return false;
-        vector<int> pattern(26,0);
+        if(s1.length() > s2.length()) return false;
+        vector<int> freqOne(26,0), freqCalc(26,0);
         for(auto x : s1) {
-            pattern[x - 'a']++;
+            freqOne[x - 'a']++;
         }
-        for(int i = 0; i < s2.length() - s1.length() + 1; i++) {
-            if(i == 0) {
-                for(int j = 0; j < s1.length(); j++) {
-                    pattern[s2[j] - 'a']--;
-                }
-                if(check(pattern))
-                    return true;
-            }
-            else {
-                pattern[s2[i + s1.length() - 1] - 'a']--;
-                pattern[s2[i-1] - 'a']++;
-                if(check(pattern))
-                    return true;
+        for(int i = 0; i < s1.length(); i++) {
+            freqCalc[s2[i] - 'a']++;
+        }
+        if(cmp(freqOne,freqCalc)) {
+            return true;
+        }
+        for(int i = s1.length(); i < s2.length(); i++) {
+            freqCalc[s2[i] - 'a']++;
+            freqCalc[s2[i-s1.length()] - 'a']--;
+            if(cmp(freqOne,freqCalc)) {
+                return true;
             }
         }
-        
         return false;
     }
 };
