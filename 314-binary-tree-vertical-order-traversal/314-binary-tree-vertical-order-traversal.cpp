a2863c93d844c59eccef18e44b0932e8c208c42d
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-13 17:48:07 -0500

    Time: 8 ms (47.72%), Space: 12.3 MB (83.17%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
@@ -12,26 +12,27 @@
 class Solution {
 public:
     
-    void dfs(TreeNode* root, int a, int &mini, int &maxi) {
-        mini = min(mini,a);
-        maxi = max(maxi,a);
-        if(root -> left != NULL) {
-            dfs(root -> left, a - 1, mini, maxi);
-        }
-        if(root -> right != NULL) {
-            dfs(root -> right, a + 1, mini, maxi);
-        }
-    }
+    // void dfs(TreeNode* root, int a, int &mini, int &maxi) {
+    //     mini = min(mini,a);
+    //     maxi = max(maxi,a);
+    //     if(root -> left != NULL) {
+    //         dfs(root -> left, a - 1, mini, maxi);
+    //     }
+    //     if(root -> right != NULL) {
+    //         dfs(root -> right, a + 1, mini, maxi);
+    //     }
+    // }
     
     vector<vector<int>> verticalOrder(TreeNode* root) {
         if(root == NULL)
             return {};
         
-        int a = 0, mini = 1e9, maxi = -1e9;
-        dfs(root, a, mini, maxi);
-        int size = maxi - mini + 1;
-        mini = -mini;
-        vector<vector<int>> ans(size);
+        // int a = 0, mini = 1e9, maxi = -1e9;
+        // dfs(root, a, mini, maxi);
+        // int size = maxi - mini + 1;
+        // mini = -mini;
+        vector<vector<int>> ans;
+        map<int,vector<int>> m;
         queue<pair<TreeNode*,int>> q;
         q.push(make_pair(root,0));
         while(!q.empty()) {
@@ -44,11 +45,15 @@ public:
                 if(curr -> right != NULL) {
                     q.push(make_pair(curr -> right, q.front().second + 1));
                 }
-                ans[q.front().second + mini].push_back(curr -> val);
+                m[q.front().second].push_back(curr -> val);
                 q.pop();
                 currlen--;
             }
         }
+        for(auto x : m) {
+            ans.push_back(x.second);
+        }
+        
         return ans;
         
     }
