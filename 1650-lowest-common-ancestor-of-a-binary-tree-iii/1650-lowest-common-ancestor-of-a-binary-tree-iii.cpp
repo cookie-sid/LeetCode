bc477ae54649b805086fae1eb5f6a62204cd82a0
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-15 16:57:17 -0500

    Time: 35 ms (10.59%), Space: 12 MB (7.78%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -12,17 +12,17 @@ public:
 class Solution {
 public:
     Node* lowestCommonAncestor(Node* p, Node * q) {
-        unordered_map<Node*,int> m;
-        m[p] = 1;
+        set<Node*> s;
+        s.insert(p);
         while(p -> parent != NULL) {
-            m[p->parent] = 1;
+            s.insert(p);
             p = p -> parent;
         }
-        if(m[q] == 1) {
+        if(s.count(q) == 1) {
             return q;
         }
         while(q -> parent != NULL) {
-            if(m[q -> parent] == 1) {
+            if(s.count(q -> parent) == 1) {
                 return q -> parent;
             }
             q = q-> parent;
