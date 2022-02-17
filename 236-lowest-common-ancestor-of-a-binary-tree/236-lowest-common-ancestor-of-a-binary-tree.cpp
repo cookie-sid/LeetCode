8549c667aa96e70533464cd38ddbac3f08ca4de2
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-16 19:23:13 -0500

    Time: 22 ms (50.70%), Space: 14.2 MB (86.63%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,44 +10,24 @@
 class Solution {
 public:
     
-    void dfs(vector<TreeNode*> &v, TreeNode* root, TreeNode* p, bool &found) {
-        if(root == p or found) {
-            found = true;
-            return;
-        }
-        if(root != p) {
-            v.push_back(root);
-        }
-        if(root -> left != NULL) {
-            dfs(v,root -> left,p,found);
-            if(!found)
-                v.pop_back();
-        }
-        if(root -> right != NULL) {
-            dfs(v,root -> right,p,found);
-            if(!found)
-                v.pop_back();
-        }
-        
-        
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        vector<TreeNode*> pathP,pathQ;
-        bool found = false;
-        dfs(pathP,root,p,found);
-        pathP.push_back(p);
-        found = false;
-        dfs(pathQ,root,q,found);
-        pathQ.push_back(q);
-        sort(pathP.begin(),pathP.end());
+        if(root == NULL or root == p or root == q)
+            return root;
         
-        for(int j = (int) pathQ.size() - 1; j >= 0; j --) {
-            if(binary_search(pathP.begin(),pathP.end(),pathQ[j]))
-                return pathQ[j];
+        auto left = lowestCommonAncestor(root -> left,p,q);
+        auto right = lowestCommonAncestor(root -> right,p,q);
+        
+        if(left != NULL and right != NULL) {
+            return root;
         }
-        return root;
+        if(left == NULL)
+            return right;
+        else 
+            return left;
+        
+        return p;
+        
         
     }
 };
