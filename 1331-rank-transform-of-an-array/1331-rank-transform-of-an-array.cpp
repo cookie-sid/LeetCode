b7031ad685599c49078486f4c2c8cd757aaa8759
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-19 11:40:30 -0500

    Time: 284 ms (8.93%), Space: 46.6 MB (14.68%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int,vector<int>> m;
-        for(int i = 0; i < arr.size(); i++) {
-            m[arr[i]].push_back(i);
+        set<int> s;
+        for(auto x : arr) {
+            s.insert(x);
+        }
+        map<int,int> ranks;
+        int rank = 1;
+        for(auto x : s) {
+            ranks[x] = rank++;
         }
         vector<int> ans(arr.size());
-        int rank = 1;
-        for(auto x : m) {
-            for(auto y : x.second)
-                ans[y] = rank;
-            
-            rank++;
+        for(int i = 0; i < arr.size(); i++) {
+            ans[i] = ranks[arr[i]];
         }
         return ans;
     }
