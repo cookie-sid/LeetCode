06409bad5cf097f14ac59d4d977e45686588ee88
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-20 14:23:00 -0500

    Time: 33 ms (53.51%), Space: 11.4 MB (59.43%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    static bool cmp(vector<int> a, vector<int> b) {
+    static bool cmp(vector<int> &a, vector<int> &b) {
         if(a[0] != b[0]) {
             return a[0] < b[0];
         }
