2957fe9e5661f6964c5eb35ab7da7d11a9a2388c
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-20 12:52:29 -0500

    Time: 136 ms (11.84%), Space: 28.7 MB (7.89%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    static bool cmp(vector<int> a, vector<int> b) {
+        if(a[0] != b[0]) {
+            return a[0] < b[0];
+        }
+        return a[1] > b[1];
+    }
+    
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(),intervals.end(),cmp);
+        
+        int start, end, ans = 0;
+        
+        for(int i = 0; i < intervals.size(); i++) {
+            if(i == 0) {
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+            else {
+                if(intervals[i][0] >= start and intervals[i][1] <= end) {
+                    ans++;
+                }
+                else {
+                    start = intervals[i][0];
+                    end = intervals[i][1];
+                }
+            }
+        }
+        return (int)intervals.size() - ans;
+        
+    }
+};
