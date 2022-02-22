36492270bad9cf92d9353f9d75788a1e1f457ac7
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-02-22 11:12:33 -0500

    Time: 96 ms (5.03%), Space: 23 MB (5.03%) - LeetHub

---
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+    vector<string> getTokens(string &s) {
+        
+        vector<string> ans;
+        int start = 0;
+        int end = s.find(":");
+        while (end != -1) {
+            ans.push_back(s.substr(start, end - start));
+            start = end + 1;
+            end = s.find(":", start);
+        }
+        ans.push_back(s.substr(start, end - start));
+        return ans;
+        
+    }
+    
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        
+        stack<pair<int,int>> st;
+        map<int,int> m;
+        vector<int> ans(n,0);
+        
+        for(auto x : logs) {
+            
+            vector<string> tmp = getTokens(x);
+            if(tmp[1] == "start") {
+                st.push(make_pair(stoi(tmp[0]),stoi(tmp[2])));
+            }
+            else {
+                
+                int id = st.top().first, starttime = st.top().second;
+                int total = 0;
+                
+                for(auto y : m) {
+                    if(y.first > starttime)
+                        total += y.second;
+                }
+                
+                int currenttime = stoi(tmp[2]) - starttime + 1 - total;
+                ans[stoi(tmp[0])] += currenttime;
+                m[starttime] = currenttime;
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
