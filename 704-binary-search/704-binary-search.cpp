3d98dddd5dff58e8b359dffabb8ee908a138c32a
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-03-08 10:50:04 -0500

    Time: 63 ms (31.80%), Space: 27.6 MB (63.92%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int start = 0, end = n - 1;
+        while(start <= end) {
+            int mid = start + end;
+            mid /= 2;
+            if(target > nums[mid]) {
+                start = mid + 1;
+            }
+            else if(target < nums[mid]) {
+                end = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+        
+    }
+};
