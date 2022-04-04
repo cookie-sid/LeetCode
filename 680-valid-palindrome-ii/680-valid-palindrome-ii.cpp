fcb1c8d59db5021bcc04e3ed325967133ffa81f9
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-04-04 11:25:48 -0400

    Time: 112 ms (5.29%), Space: 33 MB (5.06%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
-    
-    bool checkwithout(string s, int i) {
-        s = s.substr(0,i) + s.substr(i+1);
+    bool val(string s) {
         for(int i = 0; i < s.length()/2; i++) {
-            if(s[i] != s[s.length() - i - 1])
+            if(s[i] != s[(s.length()) - i - 1])
                 return false;
         }
         return true;
@@ -12,14 +10,19 @@ public:
     
     bool validPalindrome(string s) {
         
-        for(int i = 0; i < s.length()/2; i++) {
-            
-            if(s[i] != s[s.length() - 1 - i]) {
-                return checkwithout(s,i) or checkwithout(s,s.length()-1-i);
+        int ind = -1, n = s.length();
+        for(int i = 0; i < n/2; i++) {
+            if(s[i] != s[n - i - 1]) {
+                string s1 = s.substr(0,i) + s.substr(i+1);
+                string s2 = s.substr(0,n - i - 1) + s.substr(n - i);
+                if(val(s1) or val(s2)) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
-            
         }
         return true;
-        
     }
 };
