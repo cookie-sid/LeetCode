a4b7799cb25a05f24f0ec4470ef6941bfbe5a638
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-04-09 14:53:16 -0400

    Time: 14 ms (87.51%), Space: 13.7 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> temp;
+        unordered_map<int,int> m;
         int maxi = -1;
-        for(auto x : nums) {
-            temp[x]++;
-            maxi = max(maxi,temp[x]);
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+            maxi = max(maxi,m[nums[i]]);
         }
-        vector<vector<int>> bruh(maxi + 1);
-        for(auto x : temp) {
-            if(x.second != 0) {
-                bruh[x.second].push_back(x.first);
-            }
+        vector<vector<int>> count(maxi + 1);
+        for(auto x : m) {
+            count[x.second].push_back(x.first);
         }
         vector<int> ans;
         for(int i = maxi; i >= 0; i--) {
-            if(bruh[i].size() != 0) {
-                int start = 0;
-                while(ans.size() != k and start < bruh[i].size()) {
-                    ans.push_back(bruh[i][start]);
-                    start++;
+            if(count[i].size() <= k) {
+                k -= count[i].size();
+                for(auto x : count[i]) {
+                    ans.push_back(x);
                 }
             }
         }
