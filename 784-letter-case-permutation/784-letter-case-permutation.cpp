be278efc627e18ecb0750e6d554f19d4f2cead5b
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-04-25 00:03:58 -0400

    Time: 42 ms (5.69%), Space: 20.7 MB (5.05%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     
-    void recurr(string s, set<string> &v, int i) {
+    void recurr(string s, map<string,bool> &m, int i) {
         if(i == s.length()) {
             return;
         }
         string u = s.substr(0,i) + (char)(toupper(s[i])) + s.substr(i + 1);
         string l = s.substr(0,i) + (char)(tolower(s[i])) + s.substr(i + 1);
-        v.insert(u);
-        v.insert(l);
-        recurr(u,v,i+1);
-        recurr(l,v,i+1);
+        m[u] = true;
+        m[l] = true;
+        recurr(u,m,i+1);
+        recurr(l,m,i+1);
     }
     
     vector<string> letterCasePermutation(string s) {
         
-        set<string> t;
+        map<string,bool> t;
         recurr(s,t,0);
         vector<string> ans;
         for(auto x : t) {
-            ans.push_back(x);
+            ans.push_back(x.first);
         }
         return ans;
     }
