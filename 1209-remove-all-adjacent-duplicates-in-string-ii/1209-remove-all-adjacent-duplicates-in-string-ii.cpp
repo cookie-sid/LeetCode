37e4639ee62e96835858ea06f678b6091e9c9d79
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-06 15:41:35 -0400

    Time: 29 ms (60.83%), Space: 10.1 MB (69.87%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-    vector<pair<int, char>> counts;
-    for (int i = 0; i < s.size(); ++i) {
-        if (counts.empty() || s[i] != counts.back().second) {
-            counts.push_back({ 1, s[i] });
-        } else if (++counts.back().first == k) {
-            counts.pop_back();
+        stack<pair<char,int>> stk;
+        
+        for(char c : s) {
+            if(stk.empty()) {
+                stk.push({c, 1});
+            } else {
+                auto top = stk.top();
+                char prev = top.first;
+                int streak = top.second;
+                if(prev == c) {
+                    stk.pop();
+                    int newStreak = streak + 1;
+                    if(newStreak < k) {
+                        stk.push({c, newStreak});
+                    }
+                } else {
+                    stk.push({c, 1});
+                }
+            }
         }
+        
+        string ans = "";
+        while(!stk.empty()) {
+            auto top = stk.top();
+            stk.pop();
+            char c = top.first;
+            int howMany = top.second;
+            for(int i = 0; i < howMany; i++) {
+                ans += c;
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
     }
-    s = "";
-    for (auto &p : counts) {
-        s += string(p.first, p.second);
-    }
-    return s;
-}
 };
