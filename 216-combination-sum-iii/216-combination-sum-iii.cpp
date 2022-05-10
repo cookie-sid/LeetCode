9625c90896c3e3f4ebc2ad0ffc18c6a291b16834
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-10 14:41:50 -0400

    Time: 65 ms (5.36%), Space: 8.6 MB (9.22%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    void recurr(int currSum, vector<int> &curr_arr, set<vector<int>> &ans, int &k, int&n, int ctr) {
+        if(curr_arr.size() == k and currSum == n) {
+            set<int> checker; 
+            for(auto x : curr_arr) {
+                if(checker.count(x) == 1) {
+                    return;
+                }
+                checker.insert(x);
+            }
+            vector<int> temp = curr_arr;
+            sort(temp.begin(),temp.end());
+            ans.insert(temp);
+            return;
+        }
+        if(curr_arr.size() == k and currSum != n) {
+            return;
+        }
+        for(int i = ctr; i <= 9; i++) {
+            curr_arr.push_back(i);
+            recurr(currSum + i, curr_arr, ans, k, n, ctr+1);
+            curr_arr.pop_back();
+        }
+        
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        set<vector<int>> ans;
+        vector<int> curr;
+        recurr(0,curr,ans,k,n,1);
+        vector<vector<int>> ans_fin;
+        for(auto x : ans) {
+            ans_fin.push_back(x);
+        }
+        return ans_fin;
+    }
+};
