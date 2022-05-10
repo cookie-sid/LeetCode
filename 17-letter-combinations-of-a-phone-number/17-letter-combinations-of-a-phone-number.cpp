7bc0a8a25ed040b5cd971ef5053fa51b79839513
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-10 15:10:38 -0400

    Time: 0 ms (100.00%), Space: 6.5 MB (84.81%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -3,29 +3,27 @@ public:
     
     vector<string> book = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
-    vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        for(int i = 0; i < digits.length(); i++) {
-            
-            
-            int currsize = ans.size();
-            vector<string> tmp;
-            for(int j = 0; j < book[digits[i] - '0'].length(); j++) {
-                
-                if(currsize == 0) {
-                    string temp = "";
-                    tmp.push_back(temp + book[digits[i] - '0'][j]);
-                }
-                else {
-                    for(int k = 0; k < currsize; k++) {
-                        tmp.push_back(ans[k] + book[digits[i] - '0'][j]);
-                    }
-                }
-                
-            }
-            ans = tmp;
-            
+    void recurr(string &digits, int currInd, string &currStr, vector<string> &ans) {
+        if(currInd >= digits.length()) {
+            ans.push_back(currStr);
+            return;
         }
+        int temp = digits[currInd] - '0';
+        for(int i = 0; i < book[temp].length(); i++) {
+            currStr += book[temp][i];
+            recurr(digits,currInd + 1,currStr,ans);
+            currStr.pop_back();
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits.length() == 0) {
+            return {};
+        }
+        
+        vector<string> ans;
+        string blank = "";
+        recurr(digits,0,blank,ans);
         return ans;
     }
 };
