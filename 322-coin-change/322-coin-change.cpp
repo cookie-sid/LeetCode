7f5594fe6b88ef7aaeeeccf9c6c8120d08751f29
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-21 15:43:23 -0400

    Time: 380 ms (7.06%), Space: 49.2 MB (13.45%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>> knapsack(coins.size() + 1, vector<int> (amount + 1));
+        int n = coins.size();
         
-        if(amount == 0) {
-            return 0;
-        }
-        
-        vector<vector<int>> dp(coins.size() + 1, vector<int> (amount + 1,0));
-        
-        for(int i = 0; i < coins.size() + 1; i++) {
-            for(int j = 0; j < amount + 1; j++) {
-                dp[i][j] = 1e9;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= amount; j++) {
+                knapsack[0][j] = 1e9;
+                knapsack[i][j] = 1e9;
             }
         }
         
-        for(int i = 0; i < coins.size() + 1; i++) {
-            dp[i][0] = 0;
-        }
-        
-        sort(coins.begin(),coins.end());
-        
-        for(int i = 1; i <= coins.size(); i++) {
+        for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= amount; j++) {
-                
-                if(i == 1) {
-                    if(j % coins[i-1] == 0) {
-                        dp[i][j] = j / coins[i-1];
-                    }
+                if(j - coins[i-1] == 0) {
+                    knapsack[i][j] = 1;
+                }
+                else if(j - coins[i-1] > 0) {
+                    knapsack[i][j] = min(knapsack[i-1][j-coins[i-1]] + 1,
+                                        min(knapsack[i-1][j], knapsack[i][j-coins[i-1]] + 1));
                 }
                 else {
-                    dp[i][j] = min(dp[i][j], dp[i-1][j]);
-                    if(j - coins[i-1] >= 0) {
-                        dp[i][j] = min(dp[i][j], dp[i][j - coins[i-1]] + 1);
-                    }
+                    knapsack[i][j] = knapsack[i-1][j];
                 }
-                
             }
         }
-        if(dp[coins.size()][amount] >= 1e9) {
-            return -1;
-        }
-        return dp[coins.size()][amount];
+        
+        knapsack[n][amount] == 1e9 ? knapsack[n][amount] = -1 : knapsack[n][amount] = knapsack[n][amount]; 
+        return knapsack[n][amount];
     }
 };
