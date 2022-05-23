90b356bd1c8683ecd68364c3774177a9b3f4e8f2
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-23 15:14:28 -0400

    Time: 234 ms (13.12%), Space: 49.1 MB (13.21%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> knapsack(coins.size() + 1, vector<int> (amount + 1));
+        vector<vector<int>> dp(coins.size() + 1, vector<int> (amount + 1));
         int n = coins.size();
         
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= amount; j++) {
-                knapsack[0][j] = 1e9;
-                knapsack[i][j] = 1e9;
-            }
+        for(int i = 1; i <= amount; i++) {
+            dp[0][i] = 1e9;
         }
         
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= amount; j++) {
-                if(j - coins[i-1] == 0) {
-                    knapsack[i][j] = 1;
-                }
-                else if(j - coins[i-1] > 0) {
-                    knapsack[i][j] = min(knapsack[i-1][j-coins[i-1]] + 1,
-                                        min(knapsack[i-1][j], knapsack[i][j-coins[i-1]] + 1));
+                if(coins[i-1] <= j) {
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-coins[i-1]] + 1);
                 }
                 else {
-                    knapsack[i][j] = knapsack[i-1][j];
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
+        if(dp[n][amount] == 1e9) {
+            return -1;
+        }
+        return dp[n][amount];
         
-        knapsack[n][amount] == 1e9 ? knapsack[n][amount] = -1 : knapsack[n][amount] = knapsack[n][amount]; 
-        return knapsack[n][amount];
     }
 };
