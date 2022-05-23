ac948a57ba61b2240077aea14dde6d852b360a2f
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-23 00:42:54 -0400

    Time: 263 ms (80.47%), Space: 9.7 MB (83.54%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<vector<int>>> dp(strs.size() + 1, vector<vector<int>> (m + 1, vector<int>(n + 1)));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         for(int i = 1; i <= strs.size(); i++) {
             int cz = 0, co = 0;
             for(auto c : strs[i-1]) {
@@ -10,17 +10,17 @@ public:
                 else
                     co++;
             }
-            for(int j = 0; j <= m; j++) {
-                for(int k = 0; k <= n; k++) {
+            for(int j = m; j >= 0; j--) {
+                for(int k = n; k >= 0; k--) {
                     if(j - cz >= 0 and k - co >= 0) {
-                        dp[i][j][k] = max(dp[i-1][j][k],max(dp[i][j][k],dp[i-1][j-cz][k-co] + 1));
+                        dp[j][k] = max(dp[j][k],dp[j-cz][k-co] + 1);
                     }
                     else {
-                        dp[i][j][k] = max(dp[i][j][k],dp[i-1][j][k]);
+                        dp[j][k] = max(dp[j][k],dp[j][k]);
                     }
                 }
             }
         }
-        return dp[strs.size()][m][n];
+        return dp[m][n];
     }
 };
