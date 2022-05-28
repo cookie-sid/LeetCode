f0228e2ff690f09c4fae31a61c8115b2639c56d0
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-28 16:34:50 -0400

    Time: 13 ms (66.52%), Space: 11 MB (44.37%) - LeetHub

--- 818-race-car/818-race-car.cpp
+++ 818-race-car/818-race-car.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int racecar(int target) {
-        queue<vector<int>> q;
+        long long t = target;
+        queue<vector<long long>> q;
         q.push({0,0,1});
         while(!q.empty()) {
-            int moves = q.front()[0];
-            int pos = q.front()[1];
-            int vel = q.front()[2];
+            long long moves = q.front()[0];
+            long long pos = q.front()[1];
+            long long vel = q.front()[2];
             q.pop();
-            if(pos == target)
+            if(pos == t)
                 return moves;
-            if (abs(pos) > 2 * target) {
-                continue;
-            }
+            // if (abs(pos) > 2 * target) {
+            //     continue;
+            // }
             q.push({moves + 1, pos + vel, vel * 2});
             if((pos + vel > target and vel > 0) or (pos + vel < target and vel < 0))
                 q.push({moves + 1, pos, vel > 0 ? -1 : 1});
