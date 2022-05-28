ffe535c1ffbfca493d8473ce3f3238d2c86d9d38
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-27 20:59:39 -0400

    Time: 26 ms (55.22%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int sum = 0, n = nums.size();
-        for(auto x : nums) {
+        for(auto x : nums)
             sum += x;
-        }
-        return (n * (n+1))/2 - sum;
+        
+        return (n*(n+1))/2 - sum;
     }
 };
