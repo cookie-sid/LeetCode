9ada00fb15cb21c5911cdd5205671dec3ff25660
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-05-30 21:34:51 -0400

    Time: 679 ms (29.87%), Space: 98.8 MB (5.66%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,7 +4,7 @@ public:
     bool hasAllCodes(string s, int k) {
         unordered_map<string,bool> m;
         int counts = 0;
-        for(int i = 0; i < (int)s.length() - k + 1; i++) {
+        for(int i = 0; i < (int)s.length() - k + 1 and counts < pow(2,k); i++) {
             if(!m[s.substr(i,k)]) {
                 m[s.substr(i,k)] = true;
                 counts++;
