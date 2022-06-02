ec4136c57ee68b0e82416569aecf0737488258e0
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-02 16:19:30 -0400

    Added solution - LeetHub

---
+++ 7. Remove duplicates in small prime array - GFG/7.-remove-duplicates-in-small-prime-array.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    vector<int> removeDuplicate(vector<int>& arr, int n)
+    {
+        // code here
+        __int128 p = arr[0];
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for(int i = 1; i < n; i++) {
+            if(p % arr[i] != 0) {
+                p *= arr[i];
+                ans.push_back(arr[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        vector<int> A(N);
+        for(int i=0;i<N;i++)
+        {
+            cin>>A[i];
+        }
+        Solution obj;
+        vector<int>result = obj.removeDuplicate(A,N);
+        for(int i =0;i<result.size();i++)
+            cout<<result[i]<<" ";
+        cout<<endl;
+
+    }
+}
+  // } Driver Code Ends
