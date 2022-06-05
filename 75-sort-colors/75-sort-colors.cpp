086314861f3a207cf4cc73a720f009d52a70c740
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-05 14:02:17 -0400

    Time: 0 ms (100.00%), Space: 8.3 MB (70.93%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        
-        int zeropointer = 0, twopointer = nums.size() - 1;
-        
-        for(int i = 0; i < nums.size() && i <= twopointer; i++) {
-            
-            while(nums[i] == 2 and i <= twopointer) {
-                int temp = nums[twopointer];
-                nums[twopointer] = nums[i];
-                nums[i] = temp;
-                twopointer--;
+        int first = 0, last = nums.size() - 1, ctr = 0;
+        while(ctr <= last) {
+            if(nums[ctr] == 0) {
+                nums[first] = 0;
+                first += 1;
             }
-            
-            while(nums[i] == 0 and i >= zeropointer) {
-                int temp = nums[zeropointer];
-                nums[zeropointer] = nums[i];
-                nums[i] = temp;
-                zeropointer++;
+            if(nums[ctr] == 2) {
+                while(last > ctr and nums[last] == 2) {
+                    last -= 1;
+                }
+                if(nums[last] == 0) {
+                    nums[first] = 0;
+                    first += 1;
+                }
+                nums[last] = 2;
+                last -= 1;
             }
-            
+            ctr += 1;
         }
-        
-        
+        while(first <= last) {
+            nums[first] = 1;
+            first += 1;
+        }
     }
 };
