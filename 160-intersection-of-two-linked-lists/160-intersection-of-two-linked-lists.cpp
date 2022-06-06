37c5187adf2f37052a6dc8e398d1e8efbd9a7007
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-06 11:27:51 -0400

    Time: 106 ms (14.35%), Space: 14.6 MB (72.44%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -1,55 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    
-    ListNode* helper(ListNode* a, ListNode* b, int skipA, int skipB) {
-        while(skipA > 0) {
-            skipA--;
-            a = a -> next;
-        }
-        while(skipB > 0) {
-            b = b -> next;
-            skipB--;
-        }
-        while(a -> next != NULL and b -> next != NULL and a != b) {
-            a = a -> next;
-            b = b -> next;
-        }
-        if(a == b) {
-            return a;
-        }
-        return NULL;
-    }
-    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int numOfNodesInA = 1, numOfNodesInB = 1;
-        ListNode* hA = headA;
-        ListNode* hB = headB;
-        if(headA == NULL or headB == NULL)
-            return NULL;
-        while(hA -> next != NULL) {
-            numOfNodesInA++;
-            hA = hA -> next;
+        ListNode *pA = headA;
+        ListNode *pB = headB;
+        while (pA != pB) {
+            pA = pA == nullptr ? headB : pA->next;
+            pB = pB == nullptr ? headA : pB->next;
         }
-        while(hB -> next != NULL) {
-            numOfNodesInB++;
-            hB = hB -> next;
-        }
-        hA = headA;
-        hB = headB;
-        if(numOfNodesInA < numOfNodesInB) {
-            return helper(hA, hB, 0, numOfNodesInB - numOfNodesInA);
-        }
-        else if(numOfNodesInA > numOfNodesInB) {
-            return helper(hA, hB, numOfNodesInA - numOfNodesInB, 0);
-        }
-        return helper(hA, hB, 0, 0);
+        return pA;
+        // Note: In the case lists do not intersect, the pointers for A and B
+        // will still line up in the 2nd iteration, just that here won't be
+        // a common node down the list and both will reach their respective ends
+        // at the same time. So pA will be NULL in that case.
     }
 };
