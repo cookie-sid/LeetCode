8124e96b7a3dbb07d115022a2d7538db8bddfd5e
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-06 11:28:08 -0400

    Time: 88 ms (22.50%), Space: 14.4 MB (88.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -1,16 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *pA = headA;
-        ListNode *pB = headB;
-        while (pA != pB) {
-            pA = pA == nullptr ? headB : pA->next;
-            pB = pB == nullptr ? headA : pB->next;
+    
+    ListNode* helper(ListNode* a, ListNode* b, int skipA, int skipB) {
+        while(skipA > 0) {
+            skipA--;
+            a = a -> next;
         }
-        return pA;
-        // Note: In the case lists do not intersect, the pointers for A and B
-        // will still line up in the 2nd iteration, just that here won't be
-        // a common node down the list and both will reach their respective ends
-        // at the same time. So pA will be NULL in that case.
+        while(skipB > 0) {
+            b = b -> next;
+            skipB--;
+        }
+        while(a -> next != NULL and b -> next != NULL and a != b) {
+            a = a -> next;
+            b = b -> next;
+        }
+        if(a == b) {
+            return a;
+        }
+        return NULL;
+    }
+    
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int numOfNodesInA = 1, numOfNodesInB = 1;
+        ListNode* hA = headA;
+        ListNode* hB = headB;
+        if(headA == NULL or headB == NULL)
+            return NULL;
+        while(hA -> next != NULL) {
+            numOfNodesInA++;
+            hA = hA -> next;
+        }
+        while(hB -> next != NULL) {
+            numOfNodesInB++;
+            hB = hB -> next;
+        }
+        hA = headA;
+        hB = headB;
+        if(numOfNodesInA < numOfNodesInB) {
+            return helper(hA, hB, 0, numOfNodesInB - numOfNodesInA);
+        }
+        else if(numOfNodesInA > numOfNodesInB) {
+            return helper(hA, hB, numOfNodesInA - numOfNodesInB, 0);
+        }
+        return helper(hA, hB, 0, 0);
     }
 };
