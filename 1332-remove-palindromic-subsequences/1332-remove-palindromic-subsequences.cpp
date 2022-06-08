44ff85034cb15aad4b6a6219e359b94a5ea2b803
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-07 20:47:45 -0400

    Time: 0 ms (100.00%), Space: 6.2 MB (78.83%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        for (int i = 0; i < s.length()/2; i++) {
+            if(s[i] != s[s.length() - 1 - i]) {
+                return 2;
+            }
+        }
+        return 1;
+    }
+};
