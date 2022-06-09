c46c0354e694bc906daf1df88d0d48f456f8760d
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-08 23:26:42 -0400

    Time: 17 ms (63.14%), Space: 15.7 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int start = 0, end = (int)nums.size() - 1;
-        vector<int> ans;
+        int start = 0, end = nums.size() - 1;
         while(start < end) {
             if(nums[start] + nums[end] > target) {
                 end--;
@@ -11,11 +10,9 @@ public:
                 start++;
             }
             else {
-                ans.push_back(start + 1);
-                ans.push_back(end + 1);
-                break;
+                return {start + 1, end + 1};
             }
         }
-        return ans;
+        return {};
     }
 };
