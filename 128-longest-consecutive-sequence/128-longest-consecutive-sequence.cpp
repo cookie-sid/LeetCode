3bb5543a50eaeb0cd8ca03b4970e3ff5bc9c9ef8
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-15 13:41:49 -0400

    Time: 1045 ms (10.59%), Space: 49.8 MB (25.29%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int,bool> m;
+        unordered_set<int> m;
         for(auto x: nums) {
-            m[x] = true;
+            m.insert(x);
         }
         
         int ans = 0;
         for(auto x : nums) {
-            if(!m[x - 1]) {
+            if(!m.count(x - 1)) {
                 int currNum = x;
                 int currLen = 1;
-                while(m[currNum + 1]) {
+                while(m.count(currNum + 1)) {
                     currNum += 1;
                     currLen += 1;
                 }
