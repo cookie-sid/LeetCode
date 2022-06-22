82545d7fd83606b7c4e6ca8eab6c28f4bd87403a
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-22 08:18:01 -0400

    Time: 466 ms (43.43%), Space: 134.8 MB (38.40%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -3,24 +3,24 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
         vector<int> ans;
-        deque<pair<int,int>> dq;
+        deque<int> dq;
         for(int i = 0; i < k; i++) {
-            while(!dq.empty() and dq.back().first < nums[i]) {
+            while(!dq.empty() and nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
-            dq.push_back(make_pair(nums[i],i));
+            dq.push_back(i);
         }
-        ans.push_back(dq.front().first);
+        ans.push_back(nums[dq.front()]);
         
         for(int i = k; i < nums.size(); i++) {
-            if(dq.front().second < i - k + 1) {
+            if(dq.front() < i - k + 1) {
                 dq.pop_front();
             }
-            while(!dq.empty() and dq.back().first < nums[i]) {
+            while(!dq.empty() and nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
-            dq.push_back(make_pair(nums[i],i));
-            ans.push_back(dq.front().first);
+            dq.push_back(i);
+            ans.push_back(nums[dq.front()]);
         }
         return ans;
         
