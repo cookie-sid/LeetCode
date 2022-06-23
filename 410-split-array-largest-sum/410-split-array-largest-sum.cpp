31f7f915c707fbc00f45fd7cb0be305db2607aa7
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-23 17:04:28 -0400

    Time: 7 ms (32.55%), Space: 7.2 MB (71.56%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int m) {
-        int n = nums.size();
-        vector<int> prefSum(n);
-        prefSum[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            prefSum[i] += prefSum[i-1] + nums[i];
-        }
-        if(m == 1) {
-            return prefSum[n-1];
-        }
-        vector<vector<int>> dp(m + 1, vector<int>(n));
-        for(int i = 0; i < n; i++) {
-            dp[1][i] = prefSum[i];
-        }
-        for(int i = 2; i <= m; i++) {
-            int prev = i - 1;
-            for(int j = i - 1; j < n; j++) {
-                dp[i][j] = INT_MAX;
-                for(int k = prev - 1; k < j; k++) {
-                    dp[i][j] = min(dp[i][j], max(dp[i-1][k],prefSum[j] - prefSum[k]));
-                }
+    
+    int splitsReq(vector<int> &nums, int maxAllowed) {
+        int currSum = 0, splits = 0;
+        for(int i : nums) {
+            if(i + currSum <= maxAllowed) {
+                currSum += i;
+            }
+            else {
+                splits++;
+                currSum = i;
             }
         }
-        return dp[m][n-1];
+        return splits;
+    }
+    
+    int splitArray(vector<int>& nums, int m) {
+        int maxi = INT_MIN, sum = 0;
+        for(auto x : nums) {
+            maxi = max(maxi, x);
+            sum += x;
+        }
+        int left = maxi, right = sum, ans = 0;
+        while(left <= right) {
+            int mid = (left + right)/2;
+            if(splitsReq(nums,mid) + 1 <= m) {
+                right = mid - 1;
+                ans = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return ans;
     }
 };
