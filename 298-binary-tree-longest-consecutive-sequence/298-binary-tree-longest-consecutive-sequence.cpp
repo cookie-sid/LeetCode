d7fc268b7104697a16723bc2cb21514ab90e5554
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-07-05 16:33:27 -0400

    Time: 145 ms (5.12%), Space: 44.3 MB (5.12%) - LeetHub

---
+++ 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int dfs(TreeNode* root, int n, unordered_map<TreeNode*, int> &m, int &ans) {
+        m[root] = max(m[root],n);
+        ans = max(ans,m[root]);
+        if(root -> left != NULL and root -> left -> val == root -> val + 1) {
+            dfs(root -> left, m[root] + 1, m, ans);
+        }
+        if(root -> left != NULL and root -> left -> val != root -> val + 1) {
+            dfs(root -> left, 1, m, ans);
+        }
+        if(root -> right != NULL and root -> right -> val == root -> val + 1) {
+            dfs(root -> right, m[root] + 1, m, ans);
+        }
+        if(root -> right != NULL and root -> right -> val != root -> val + 1) {
+            dfs(root -> right, 1, m, ans);
+        }
+        return ans;
+    }
+    
+    int longestConsecutive(TreeNode* root) {
+        int ans = 1;
+        unordered_map<TreeNode*, int> m;
+        return dfs(root,1,m,ans);
+    }
+};
