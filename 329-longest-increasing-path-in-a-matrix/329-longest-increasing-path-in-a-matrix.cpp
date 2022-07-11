689b5a188e7732532502fc16fcfa51b2b9f7f6b2
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-07-11 18:58:41 -0400

    Time: 56 ms (74.16%), Space: 16 MB (68.48%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
     
-    int recurr(vector<vector<int>> &matrix, int &maxlen, int x, int y, int &m, int &n, int currlen, vector<vector<int>> &ans) {
-        if(x < 0 or x > m - 1 or y < 0  or y > n - 1) {
-            return 0;
-        }
-        maxlen = max(maxlen, currlen);
+    bool valid(int i, int j, int n, int m) {
+        return i >= 0 and i < n and j >= 0 and j < m;
+    }
+    
+    int longest(vector<vector<int>> &matrix, int curri, int currj, int n, int m, vector<vector<int>> &dp) {
         
-        if(ans[x][y] != 0) {
-            return ans[x][y];
+        if(dp[curri][currj] != 0) {
+            return dp[curri][currj];
         }
-        
-        int maxi = 0;
-        if(!(x < 0 or x > m - 1 or y < 0  or y > n - 1) and
-           !(x + 1 < 0 or x + 1 > m - 1) and matrix[x + 1][y] > matrix[x][y]) {
-            maxi = max(maxi,1 + recurr(matrix,maxlen,x+1,y,m,n,currlen + 1,ans));
+        int maxi = 1;
+        if(valid(curri+1,currj,n,m) and matrix[curri+1][currj] > matrix[curri][currj]) {
+            maxi = max(maxi,1 + longest(matrix,curri + 1,currj,n,m,dp));
         }
-        if(!(x < 0 or x > m - 1 or y < 0  or y > n - 1) and
-           !(x - 1 < 0 or x - 1 > m - 1) and matrix[x - 1][y] > matrix[x][y]) {
-            maxi = max(maxi,1 + recurr(matrix,maxlen,x-1,y,m,n,currlen + 1,ans));
+        if(valid(curri-1,currj,n,m) and matrix[curri-1][currj] > matrix[curri][currj]) {
+            maxi = max(maxi,1 + longest(matrix,curri - 1,currj,n,m,dp));
         }
-        if(!(x < 0 or x > m - 1 or y < 0  or y > n - 1) and
-           !(y + 1 < 0 or y + 1 > n - 1) and matrix[x][y + 1] > matrix[x][y]) {
-            maxi = max(maxi,1 + recurr(matrix,maxlen,x,y+1,m,n,currlen + 1,ans));
+        if(valid(curri,currj+1,n,m) and matrix[curri][currj+1] > matrix[curri][currj]) {
+            maxi = max(maxi,1 + longest(matrix,curri,currj + 1,n,m,dp));
         }
-        if(!(x < 0 or x > m - 1 or y < 0  or y > n - 1) and
-           !(y - 1 < 0 or y - 1 > n - 1) and matrix[x][y - 1] > matrix[x][y]) {
-            maxi = max(maxi, 1 + recurr(matrix,maxlen,x,y-1,m,n,currlen + 1,ans));
+        if(valid(curri,currj-1,n,m) and matrix[curri][currj-1] > matrix[curri][currj]) {
+            maxi = max(maxi,1 + longest(matrix,curri,currj - 1,n,m,dp));
         }
-        ans[x][y] = max(ans[x][y],maxi);
+        dp[curri][currj] = maxi;
         return maxi;
+        
     }
     
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int maxlen = 1;
-        int temp = -1;
-        vector<vector<int>> ans(m, vector<int> (n));
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                temp = max(temp,1 + recurr(matrix,maxlen,i,j,m,n,1,ans));
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int> (m));
+        int maxi = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(dp[i][j] == 0) {
+                    dp[i][j] = longest(matrix,i,j,n,m,dp);
+                }
+                maxi = max(maxi,dp[i][j]);
             }
         }
-        
-        
-        return temp;
+        return maxi;
     }
 };
