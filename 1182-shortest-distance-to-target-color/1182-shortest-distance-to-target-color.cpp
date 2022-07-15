453af1f48839d40a630302802b4384067d829c83
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-07-15 13:03:59 -0400

    Time: 792 ms (17.58%), Space: 122.6 MB (84.34%) - LeetHub

---
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
+        vector<vector<int>> colorIndices(4);
+        for(int i = 0; i < colors.size(); i++) {
+            colorIndices[colors[i]].push_back(i);
+        }
+        vector<int> ans;
+        for(int i = 0; i < queries.size(); i++) {
+            int index = queries[i][0];
+            int reqColor = queries[i][1];
+            int lower = lower_bound(colorIndices[reqColor].begin(),colorIndices[reqColor].end(),index) - colorIndices[reqColor].begin();
+            int upper = upper_bound(colorIndices[reqColor].begin(),colorIndices[reqColor].end(),index) - colorIndices[reqColor].begin();
+            int currAns = 1e9;
+            if(colorIndices[reqColor].size() == 0) {
+                ans.push_back(-1);
+                continue;
+            }
+            if(colors[index] == reqColor) {
+                ans.push_back(0);
+                continue;
+            }
+            if(lower != 0) {
+                currAns = min(index - colorIndices[reqColor][lower-1],currAns);
+            }
+            if(upper != (int)colorIndices[reqColor].size()) {
+                currAns = min(colorIndices[reqColor][upper] - index,currAns);
+            }
+            ans.push_back(currAns);
+        }
+        return ans;
+    }
+};
