c7cd32a0d5e934f7c7594b90a661f2d57093e20b
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-07-18 15:13:10 -0400

    Time: 165 ms (32.66%), Space: 50 MB (7.10%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> m;
-        int count = 0, sum = 0;
-        for(auto x : nums) {
-            sum += x;
-            if(sum == k)
-                count++;
-            count += m[sum - k];
-            m[sum]++;
+        map<int,int> m;
+        
+        m[0] = 1;
+        int sum = 0, ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if(m[sum - k]) {
+                ans += m[sum - k];
+            }
+            m[sum] += 1;
         }
-        return count;
+        return ans;
+        
     }
 };
