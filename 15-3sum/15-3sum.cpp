e016e53b9d65c8b9a68d9e2884c54434f56b4e68
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-07-27 18:47:38 -0400

    Time: 2961 ms (5.02%), Space: 477 MB (5.41%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     
+    vector<vector<int>> twoSum(vector<int> &nums, int start, int end, int target) {
+        map<int,int> m;
+        vector<vector<int>> ans;
+        if(nums[start] + nums[start + 1] > target or nums[end] + nums[end - 1] < target) {
+            return ans;
+        }
+        for(int i = start; i <= end; i++) {
+            if(m[target - nums[i]] > 0) {
+                ans.push_back({min(target - nums[i],nums[i]),max(target - nums[i],nums[i])});
+            }
+            m[nums[i]]++;
+        }
+        return ans;
+    }
     
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        if(nums.size() < 3) {
-            return {};
-        }
+        set<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
+        int n = nums.size() - 1;
         for(int i = 0; i < nums.size() - 2; i++) {
-            
-            int l = i + 1, r = nums.size() - 1;
-            while(l < r) {
-                if(nums[l] + nums[r] < -nums[i]) {
-                    l++;
-                }
-                else if(nums[l] + nums[r] > -nums[i]) {
-                    r--;
-                }
-                else {
-                    ans.push_back({nums[i],nums[l],nums[r]});
-                    while(l + 1 < r and nums[l + 1] == nums[l]) {
-                        l++;
-                    }
-                    while(r - 1 > l and nums[r - 1] == nums[r]) {
-                        r--;
-                    }
-                    l++;
-                    r--;
-                }
+            vector<vector<int>> tmp = twoSum(nums,i + 1,n,-nums[i]);
+            for(auto x : tmp) {
+                vector<int> t = x;
+                t.push_back(nums[i]);
+                sort(t.begin(),t.end());
+                ans.insert(t);
             }
-            
-            while(i < nums.size() - 2 and nums[i] == nums[i+1]) {
+            while(i + 1 < nums.size() and nums[i] == nums[i+1]) {
                 i++;
             }
         }
-        return ans;
+        vector<vector<int>> res;
+        for(auto x : ans) {
+            res.push_back(x);
+        }
+        return res;
     }
 };
