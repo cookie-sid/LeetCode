f4934f7955f6d7b892591bbd2e9d8d5b9eda0e11
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-07-29 17:28:28 -0400

    Time: 824 ms (70.89%), Space: 63.1 MB (25.88%) - LeetHub

--- 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.py
+++ 2338-count-the-number-of-ideal-arrays/2338-count-the-number-of-ideal-arrays.py
@@ -1,19 +1,21 @@
 class Solution:
-    def idealArrays(self, n: int, mx: int) -> int:
+    def idealArrays(self, n: int, maxValue: int) -> int:
         
         @lru_cache(None)
-        def gen(k):            
-            return math.comb(n-1, k-1)
-
+        def gen(k):
+            return math.comb(n-1,k-1);
+        
         @lru_cache(None)
-        def dp(cur, l):
-            res = gen(l)
-            nxt = cur * 2
-            if l == n or nxt > mx:
+        def dp(val,length):
+            res = gen(length)
+            tmp = 2*val
+            if(length == n or tmp > maxValue):
                 return res
-            while nxt <= mx:
-                res += dp(nxt, l+1)
-                nxt += cur
+            while(tmp <= maxValue):
+                res += dp(tmp,length+1)
+                tmp += val
+            
             return res
+        
+        return sum([dp(i,1) for i in range(1,maxValue+1)]) % (10**9 + 7)
                 
-        return sum([dp(i, 1) for i in range(1, mx+1)]) % (10**9 + 7)
