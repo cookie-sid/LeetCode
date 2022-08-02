0cdb4f432115cf0da08088c641dc7b2b1006aacd
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-01 22:27:27 -0400

    Time: 147 ms (76.67%), Space: 14.9 MB (21.69%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int r = matrix.size() - 1;
-        int c = 0;
-        while(r >= 0 and c < matrix[0].size()) {
-            if(matrix[r][c] > target) {
-                r--;
+        int m = matrix.size(), n = matrix[0].size();
+        int x = 0, y = n - 1;
+        while(x >= 0 and y >= 0 and x < m and y < n) {
+            if(matrix[x][y] > target) {
+                y--;
             }
-            else if (matrix[r][c] < target) {
-                c++;
+            else if(matrix[x][y] < target) {
+                x++;
             }
             else {
                 return true;
             }
         }
         return false;
-   }
+    }
 };
