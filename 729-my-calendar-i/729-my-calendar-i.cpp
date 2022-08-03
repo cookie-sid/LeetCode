eca7f7b14db98e3bb4cf372f97b22673abc6d8f0
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-02 21:52:14 -0400

    Time: 171 ms (48.84%), Space: 37.7 MB (87.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,15 +1,30 @@
 class MyCalendar {
 public:
-    MyCalendar() {}
     
-    bool book(int start, int end) {
-        for (auto [s, e] : calendar)
-            if (max(s, start) < min(e, end)) return false;
+    vector<pair<int,int>> v;
+    
+    MyCalendar() {
         
-        calendar.push_back({start, end});
-        return true;
     }
     
-private:
-    vector<pair<int, int>> calendar;
+    bool book(int start, int end) {
+        bool ans = true;
+        for(auto x : v) {
+            if(((start >= x.first and start < x.second) or (end - 1 >= x.first and end - 1 < x.second))
+              or ((x.first >= start and x.first < end) or (x.second - 1 >= start and x.second - 1 < end)))
+                return false;
+            if(!ans)
+                break;
+        }
+        if(ans) {
+            v.push_back(make_pair(start,end));
+        }
+        return ans;
+    }
 };
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
