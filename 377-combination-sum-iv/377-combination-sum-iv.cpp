55f828f957fbea544a72a41b02045ce16a1630a6
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-05 00:42:48 -0400

    Time: 3 ms (62.90%), Space: 6.6 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,13 +1,20 @@
-
-
 class Solution {
 public:
-    int combinationSum4(vector<int>& N, int T) {
-        vector<unsigned int> dp(T+1, 0);
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<unsigned int> dp(target + 1,0);
         dp[0] = 1;
-        for (int i = 1; i <= T; i++)
-            for (int num : N)
-                if (num <= i) dp[i] += dp[i-num];
-        return dp[T];
+        for(int i = 1; i <= target; i++) {
+            for(int j = 0; j < nums.size(); j++) {
+                if(i - nums[j] >= 0) {
+                    dp[i] += dp[i - nums[j]];
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        return dp[target];
     }
 };
