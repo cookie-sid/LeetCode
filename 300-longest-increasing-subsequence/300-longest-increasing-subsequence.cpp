4eb42879fb3a8d393939a56e1cbec035138fa061
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-08 10:53:43 -0400

    Time: 548 ms (30.94%), Space: 10.6 MB (28.08%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,1);
+        dp[0] = 1;
+        int maxi = 1;
+        for(int i = 1; i < n; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = max(dp[i],dp[j] + 1);
+                }
+            }
+        }
+        for(auto x : dp) {
+            maxi = max(maxi,x);
+        }
+        return maxi;
+    }
+};
