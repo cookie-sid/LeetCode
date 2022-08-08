04b892af612b1529ba3a1343d9a8bfbf008b4c07
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-08 16:48:19 -0400

    Time: 746 ms (73.74%), Space: 97.7 MB (86.82%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.cpp
+++ 827-making-a-large-island/827-making-a-large-island.cpp
@@ -1,60 +1,62 @@
 class Solution {
 public:
     
-    void helper(vector<vector<int>> &grid, vector<vector<int>> &taken, int x, int y, int n, int val) {
-        if(x < 0 or x >= n or y < 0 or y >= n) {
-            return;
+    int dfs(vector<vector<int>> &grid, int x, int y, int r, int c, int id) {
+        if(x < 0 or x >= r or y < 0 or y >= c) {
+            return 0;
         }
-        if(taken[x][y] != 0 or grid[x][y] == 0)
-            return;
-        taken[x][y] = val;
-        helper(grid,taken,x + 1,y,n,val);
-        helper(grid,taken,x,y + 1,n,val);
-        helper(grid,taken,x - 1,y,n,val);
-        helper(grid,taken,x,y - 1,n,val);
+        if(grid[x][y] == 0 or grid[x][y] != 1) {
+            return 0;
+        }
+        grid[x][y] = id;
+        int ans = 1;
+        ans += dfs(grid,x + 1,y,r,c,id);
+        ans += dfs(grid,x,y + 1,r,c,id);
+        ans += dfs(grid,x - 1,y,r,c,id);
+        ans += dfs(grid,x,y - 1,r,c,id);
+        return ans;
     }
     
+    
+    
     int largestIsland(vector<vector<int>>& grid) {
-        
-        int n = grid.size();
-        vector<vector<int>> taken(n, vector<int> (n,0));
-        int val = 1;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1 and taken[i][j] == 0) {
-                    helper(grid,taken,i,j,n,val);
-                    val++;
+        int r = grid.size(), c = grid[0].size();
+        map<int,int> m;
+        int id = 2, ans = 0;
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++) {
+                if(grid[i][j] == 1) {
+                    m[id] = dfs(grid,i,j,r,c,id);
+                    ans = max(ans,m[id]);
+                    id += 1;
                 }
-            }
-        } 
-        unordered_map<int,int> m;
-        m[0] = 0;
-        int maxi = 0;
-        for(auto x : taken) {
-            for(auto y : x) {
-                if(y == 0)
-                    continue;
-                m[y]++;
-                maxi = max(maxi,m[y]);
             }
         }
         
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++) {
                 if(grid[i][j] == 0) {
                     set<int> s;
-                    s.insert(taken[max(i - 1,0)][j]);
-                    s.insert(taken[min(i + 1,n - 1)][j]);
-                    s.insert(taken[i][min(j + 1, n - 1)]);
-                    s.insert(taken[i][max(j - 1,0)]);
+                    if(i > 0 and grid[i-1][j] != 0) {
+                        s.insert(grid[i-1][j]);
+                    }
+                    if(j > 0 and grid[i][j-1] != 0) {
+                        s.insert(grid[i][j-1]);
+                    }
+                    if(i < r - 1 and grid[i+1][j] != 0) {
+                        s.insert(grid[i+1][j]);
+                    }
+                    if(j < c - 1 and grid[i][j+1] != 0) {
+                        s.insert(grid[i][j+1]);
+                    }
                     int sum = 0;
                     for(auto x : s) {
                         sum += m[x];
                     }
-                    maxi = max(maxi,sum + 1);
+                    ans = max(ans,sum + 1);
                 }
             }
         }
-        return maxi;
+        return ans;
     }
 };
