2097c13a079326fd7a2a6b0194aaca329d840284
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-10 23:07:25 -0400

    Time: 13 ms (18.59%), Space: 12.5 MB (22.32%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
@@ -12,49 +12,31 @@
 class Solution {
 public:
     
-    // void dfs(TreeNode* root, int a, int &mini, int &maxi) {
-    //     mini = min(mini,a);
-    //     maxi = max(maxi,a);
-    //     if(root -> left != NULL) {
-    //         dfs(root -> left, a - 1, mini, maxi);
-    //     }
-    //     if(root -> right != NULL) {
-    //         dfs(root -> right, a + 1, mini, maxi);
-    //     }
-    // }
-    
     vector<vector<int>> verticalOrder(TreeNode* root) {
-        if(root == NULL)
+        if(!root) {
             return {};
-        
-        // int a = 0, mini = 1e9, maxi = -1e9;
-        // dfs(root, a, mini, maxi);
-        // int size = maxi - mini + 1;
-        // mini = -mini;
+        }
         vector<vector<int>> ans;
         map<int,vector<int>> m;
         queue<pair<TreeNode*,int>> q;
         q.push(make_pair(root,0));
         while(!q.empty()) {
-            int currlen = q.size();
-            while(currlen > 0) {
-                TreeNode* curr = q.front().first;
-                if(curr -> left != NULL) {
-                    q.push(make_pair(curr -> left, q.front().second - 1));
-                }
-                if(curr -> right != NULL) {
-                    q.push(make_pair(curr -> right, q.front().second + 1));
-                }
-                m[q.front().second].push_back(curr -> val);
+            int csize = q.size();
+            for(int i = 0; i < csize; i++) {
+                auto top = q.front();
                 q.pop();
-                currlen--;
+                m[top.second].push_back(top.first -> val);
+                if(top.first -> left != NULL) {
+                    q.push(make_pair(top.first -> left, top.second - 1));
+                }
+                if(top.first -> right != NULL) {
+                    q.push(make_pair(top.first -> right, top.second + 1));
+                }
             }
         }
         for(auto x : m) {
             ans.push_back(x.second);
         }
-        
         return ans;
-        
     }
 };
