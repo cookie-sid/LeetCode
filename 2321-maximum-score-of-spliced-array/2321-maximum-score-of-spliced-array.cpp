6d48858d7aaf795ed38392e59273c7df4cd438c2
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-13 17:02:58 -0400

    Time: 147 ms (95.96%), Space: 86.3 MB (36.64%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     
-    int getWorst(vector<int> &arr) {
-        int worst = 0, total = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            total + arr[i] < 0 ? total += arr[i] : total = 0;
-            worst = min(worst,total);
-        }
-        return worst;
-    }
+     int funct(vector<int> &nums1, vector<int> &nums2, int n) {
+         vector<int> tmp(n);
+         int total = 0;
+         for(int i = 0; i < n; i++) {
+             tmp[i] = nums1[i] - nums2[i];
+             total += nums1[i];
+         }
+         int worst = 0, curr = 0;
+         for(int i = 0; i < n; i++) {
+             curr = min(tmp[i],curr + tmp[i]);
+             worst = min(worst,curr);
+         }
+         if(worst > 0) {
+             return total;
+         }
+         return total - worst;
+     }
     
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        vector<int> diff(n);
-        int sum1 = 0, sum2 = 0;
-        for(int i = 0; i < n; i++) {
-            sum1 += nums1[i];
-            sum2 += nums2[i];
-            diff[i] = nums1[i] - nums2[i];
-        }
-        int ans = max(sum1,sum2);
-        ans = max(ans,sum1 - getWorst(diff));
-        for(int i = 0; i < n; i++) {
-            diff[i] *= -1;
-        }
-        ans = max(ans,sum2 - getWorst(diff));
-        return ans;
+        return max(funct(nums1,nums2,n),funct(nums2,nums1,n));
     }
+    
 };
