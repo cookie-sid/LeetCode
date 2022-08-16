94f71acd95e4c83a1ab231b0053b268d6ef2c754
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-16 16:28:29 -0400

    Time: 99 ms (91.66%), Space: 26.7 MB (72.43%) - LeetHub

--- 975-odd-even-jump/975-odd-even-jump.cpp
+++ 975-odd-even-jump/975-odd-even-jump.cpp
@@ -4,30 +4,27 @@ public:
     int oddEvenJumps(vector<int>& arr) {
         int n = arr.size();
         map<int,int> m;
-        set<int> s;
         vector<pair<bool,bool>> dp(n);
         dp[n-1].first = true;
         dp[n-1].second = true;
         m[arr[n-1]] = n;
-        s.insert(arr[n-1]);
         for(int i = n - 2; i >= 0; i--) {
-            auto it = s.lower_bound(arr[i]);
-            if(it == s.end()) {
+            auto it = m.lower_bound(arr[i]);
+            if(it == m.end()) {
                 dp[i].second = false;
             }
             else {
-                dp[i].second = dp[m[(*it)] - 1].first;
+                dp[i].second = dp[it -> second - 1].first;
             }
-            if(it != s.end() and (*it) == arr[i]) {
-                dp[i].first = dp[m[(*it)] - 1].second;
+            if(it != m.end() and it -> first == arr[i]) {
+                dp[i].first = dp[it -> second - 1].second;
             }
             else {
-                if(it != s.begin()) {
+                if(it != m.begin()) {
                     it--;
-                    dp[i].first = dp[m[(*it)] - 1].second;
+                    dp[i].first = dp[it -> second - 1].second;
                 }
             }
-            s.insert(arr[i]);
             m[arr[i]] = i + 1;
         }
         int ans = 0;
