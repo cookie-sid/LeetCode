b450c95f0d1ae67a8dbf73027af3be7e6ae90fb2
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-16 11:55:43 -0400

    Time: 221 ms (96.97%), Space: 166.1 MB (94.69%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cpp
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cpp
@@ -2,30 +2,38 @@ class SparseVector {
 public:
     
     vector<pair<int,int>> v;
+    int size;
     
     SparseVector(vector<int> &nums) {
+        
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] != 0) {
-                this -> v.push_back(make_pair(nums[i],i));
+                v.push_back(make_pair(nums[i],i));
             }
         }
+        size = v.size();
+        
     }
     
     // Return the dotProduct of two sparse vectors
     int dotProduct(SparseVector& vec) {
         int start = 0, ans = 0;
-        for(int i = 0; i < (int)this -> v.size() and start < (int)vec.v.size(); i++) {
-            if(this -> v[i].second > vec.v[start].second) {
+        for(int i = 0; i < vec.size and start < size; i++) {
+            if(vec.v[i].second < v[start].second) {
+                continue;
+            }
+            else if(vec.v[i].second == v[start].second) {
+                ans += vec.v[i].first * v[start].first;
+                start++;
+            }
+            else {
                 start++;
                 i--;
-            }
-            else if(this -> v[i].second == vec.v[start].second) {
-                ans += this -> v[i].first * vec.v[start].first;
-                start++;
             }
         }
         return ans;
     }
+    
 };
 
 // Your SparseVector object will be instantiated and called as such:
