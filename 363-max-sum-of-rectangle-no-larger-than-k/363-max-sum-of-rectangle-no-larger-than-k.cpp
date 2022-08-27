05073277ab86ccaa6c8a98aea822a45ec75fd38e
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-08-27 14:20:38 -0400

    Time: 2294 ms (13.15%), Space: 290.1 MB (70.59%) - LeetHub

---
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+        int m = matrix.size(), n = matrix[0].size();
+        for(int i = 0; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                matrix[i][j] += matrix[i][j-1];
+            }
+        }
+        int maxi = -1e9;
+        for(int i = 0; i < n; i++) {
+            for(int j = -1; j < i; j++) {
+                int currSum = 0;
+                set<int> s;
+                for(int l = 0; l < m; l++) {
+                    int sub = 0;
+                    if(j != -1) {
+                        sub = matrix[l][j];
+                    }
+                    currSum += (matrix[l][i] - sub);
+                    if(currSum <= k)
+                        maxi = max(currSum,maxi);
+                    if(s.size() == 0) {
+                        s.insert(currSum);
+                    }
+                    else {
+                        int exact = currSum - k;
+                        auto itr = s.lower_bound(exact);
+                        if(itr != s.end()) {
+                            maxi = max(maxi,currSum - *itr);
+                        }
+                        s.insert(currSum);
+                    }
+                }
+                
+            }
+        }
+        return maxi;
+    } 
+};
