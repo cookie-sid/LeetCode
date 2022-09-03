91a5b8e0c6570106ecd4769145eb22ad97280d4e
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-09-03 17:35:31 -0400

    Time: 33 ms (34.41%), Space: 17.6 MB (35.56%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     
-    void recur(int currtotal, int &total, vector<int> & candidates, set<vector<int>>&ans, vector<int>& current) {
-        if(currtotal == total) {
-            vector<int> temp = current;
-            sort(temp.begin(),temp.end());
-            ans.insert(temp);
+    void recur(int currtotal, int &total, vector<int> &candidates, set<vector<int>>&ans, vector<int>& current, int i) {
+        if(i == candidates.size()) {
             return;
         }
         if(currtotal > total) {
             return;
         }
-        
-        for(auto x : candidates) {
-            current.push_back(x);
-            recur(currtotal + x,total,candidates,ans,current);
-            current.pop_back();
+        if(currtotal == total) {
+            ans.insert(current);
         }
+        recur(currtotal,total,candidates,ans,current,i+1);
+        current.push_back(candidates[i]);
+        recur(currtotal + candidates[i],total,candidates,ans,current,i);
+        current.pop_back();
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
+        sort(candidates.begin(),candidates.end());
         vector<vector<int>> res;
         set<vector<int>> ans;
         vector<int> current;
-        recur(0,target,candidates,ans,current);
+        recur(0,target,candidates,ans,current,0);
         for(auto x : ans) {
             res.push_back(x);
         }
