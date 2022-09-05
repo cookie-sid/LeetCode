66b678c201e7e85122926dc9a092552c7ac455f6
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-09-05 13:56:22 -0400

    Time: 3502 ms (11.11%), Space: 60.3 MB (11.11%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -3,57 +3,79 @@ from heapq import heapify, heappush, heappop
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         meetings.sort()
-        rooms = []
+        # print(meetings)
+        unocc,occ = [],[]
         
         for i in range(n):
-            rooms.append([-1 ,i])
+            unocc.append(i)
             
-        maxi = [0]*n
-        heapify(rooms)
-        
-        for m in meetings:
-            # print(rooms)
-            ls = heappop(rooms)
-            endTime = ls[0]
-            rnum = ls[1]
-            if endTime == -2 :
-                heappush(rooms,[m[1],rnum])
-                maxi[rnum] += 1
+        count = [0]*n
+        heapify(occ)
+        heapify(unocc)
+        for meeting in meetings:
+            if len(occ) == 0:
+                top = heappop(unocc)
+                heappush(occ,[meeting[1],top])
+                count[top] += 1
             else:
-                b = []
-                heappush(rooms,ls)
-                mini = 102
-                while len(rooms) > 0:
-                    ele = heappop(rooms)
-                    if(ele[0] > m[0]):
-                        heappush(rooms,ele)
-                        break
-                    else:
-                        b.append(ele)
-                        mini = min(mini,ele[1])
-                
-                if mini == 102:
-                    ele = heappop(rooms)
-                    heappush(rooms,[m[1] + ele[0] - m[0],ele[1]])
-                    maxi[ele[1]] += 1
-                else:
-                    maxi[mini] += 1
-                    for ele in b:
-                        if ele[1] == mini:
-                            heappush(rooms,[m[1],ele[1]])
+                if len(unocc) == 0:
+                    mini = 102
+                    ls = []
+                    while len(occ) != 0:
+                        topRoom = occ[0]
+                        if topRoom[0] <= meeting[0] :
+                            mini = min(mini,topRoom[1])
+                            ls.append(occ[0])
+                            heappop(occ)
                         else:
-                            heappush(rooms,[-1,ele[1]])
-                    
-            
-        top = 0
+                            break
+                    if mini == 102 :
+                        topRoom = heappop(occ)
+                        heappush(occ,[meeting[1] + topRoom[0] - meeting[0],topRoom[1]])
+                        count[topRoom[1]] += 1
+                    else:
+                        heappush(occ,[meeting[1],mini])
+                        count[mini] += 1
+                        for ele in ls:
+                            if ele[1] != mini:
+                                heappush(unocc,ele[1])
+                else:
+                    firstEmpty = unocc[0]
+                    mini = 102
+                    ls = []
+                    while len(occ) != 0:
+                        topRoom = occ[0]
+                        if topRoom[0] <= meeting[0] :
+                            mini = min(mini,topRoom[1])
+                            ls.append(occ[0])
+                            heappop(occ)
+                        else:
+                            break
+                    if mini == 102 :
+                        tR = heappop(unocc)
+                        heappush(occ,[meeting[1],tR])
+                        count[tR] += 1
+                    else:
+                        if(mini > firstEmpty):
+                            mini = firstEmpty
+                            heappop(unocc)
+                        heappush(occ,[meeting[1],mini])
+                        count[mini] += 1
+                        for ele in ls:
+                            if ele[1] != mini:
+                                heappush(unocc,ele[1])
+            # for i in range(0,n):
+            #     if(count[i] == 0):
+            #         print(count[0:i])
+            #         break
+            # print(occ)
+            # print(unocc)
+        # print(count)
+        maxi = 0
         num = 0
-        for i in range(0,n):
-            
-            
-            if(top < maxi[i]):
-                top = maxi[i]
+        for i in range(n):
+            if count[i] > maxi:
+                maxi = count[i]
                 num = i
                 
         return num
-                    
-            
