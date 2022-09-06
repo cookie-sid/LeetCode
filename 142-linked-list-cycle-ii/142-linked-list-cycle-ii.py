62e80b8ebc9cf22b69287871735d1425bfac65d5
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-09-06 13:33:49 -0400

    Time: 109 ms (16.25%), Space: 17.5 MB (34.61%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -0,0 +1,64 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head:
+            return None
+        
+        slow = head
+        fast = head
+        if not fast.next:
+            return None
+        else:
+            fast = fast.next
+        if not fast.next:
+            return None
+        else:
+            fast = fast.next
+        firstMeetsAtNode = None
+        ds = 0
+        df = 2
+        while slow.next and fast.next:
+            # print(slow)
+            # print(fast)
+            if slow == fast :
+                firstMeetsAt = slow
+                break
+            ds += 1
+            df += 2
+            slow = slow.next
+            fast = fast.next
+            if not fast.next:
+                break
+            else:
+                fast = fast.next
+            
+        if not slow.next or not fast.next:
+            return None
+            
+        steps = df - ds
+        # print(df)
+        # print(ds)
+        # print(steps)
+        slow = head
+        fast = head
+        c = 0
+        p = 0
+        while c != steps:
+            fast = fast.next
+            c += 1
+        while slow != fast:
+            p += 1
+            slow = slow.next
+            fast = fast.next
+        
+        return slow
+        
+        
+        
