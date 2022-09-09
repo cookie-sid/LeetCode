6f467198e47a28c919ed7a8c6390075b48eed368
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-09-09 00:23:07 -0400

    Time: 6007 ms (5.04%), Space: 67.1 MB (63.30%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,24 +1,20 @@
 class Solution:
-    def numberOfWeakCharacters(self, p: List[List[int]]) -> int:
-        p.sort()
-        pmax = p[len(p) - 1][0]
-        maxi = p[len(p) - 1][1]
-        n = len(p)
-        lp = pmax
+    def numberOfWeakCharacters(self, prop: List[List[int]]) -> int:
+        
+        maxD = [0]*100002
+        
+        for p in prop:
+            maxD[p[0]] = max(maxD[p[0]],p[1])
+            
+            
+        for i in range(100000,-1,-1):
+            maxD[i] = max(maxD[i],maxD[i+1])
+            
         ans = 0
-        newMaxi = -1
-        for i in range(n-2,-1,-1):
-            if p[i][0] == lp:
-                if lp == pmax:
-                    continue
-                else:
-                    ans += 1 if p[i][1] < maxi else 0
-                    newMaxi = max(newMaxi,p[i][1])
-            else:
-                maxi = max(maxi,newMaxi)
-                ans += 1 if p[i][1] < maxi else 0
-                lp = p[i][0]
-                newMaxi = p[i][1]
+        for p in prop:
+            if maxD[p[0]+1] > p[1]:
+                ans += 1
                 
         return ans
-        
+            
+                    
