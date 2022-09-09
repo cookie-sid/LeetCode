10451a457cd9120b0045d7578bf77b2945a69590
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-09-09 00:24:37 -0400

    Time: 4756 ms (9.73%), Space: 67.6 MB (51.93%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,18 +1,22 @@
 class Solution:
     def numberOfWeakCharacters(self, prop: List[List[int]]) -> int:
         
-        maxD = [0]*100002
+        maxiA = -1
+        for p in prop:
+            maxiA = max(maxiA,p[0])
+        
+        maxD = [0]*(maxiA+1)
         
         for p in prop:
             maxD[p[0]] = max(maxD[p[0]],p[1])
             
             
-        for i in range(100000,-1,-1):
+        for i in range(maxiA-1,-1,-1):
             maxD[i] = max(maxD[i],maxD[i+1])
             
         ans = 0
         for p in prop:
-            if maxD[p[0]+1] > p[1]:
+            if p[0] != maxiA and maxD[p[0]+1] > p[1]:
                 ans += 1
                 
         return ans
