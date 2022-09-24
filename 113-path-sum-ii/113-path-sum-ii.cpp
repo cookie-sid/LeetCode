acc13e6d1173248dbd8108566a8e975dfbf923a3
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-09-24 11:51:24 -0400

    Time: 27 ms (37.50%), Space: 19.8 MB (81.15%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,30 +12,34 @@
 class Solution {
 public:
     
-    void dfs(TreeNode* root, int &targetSum, int currSum, vector<int> &path, vector<vector<int>> &ans) {
-        if(root -> left == NULL and root -> right == NULL and currSum + root -> val == targetSum) {
-            path.push_back(root->val);
-            ans.push_back(path);
-            path.pop_back();
+    void dfs(TreeNode* root, int total, int target, vector<int> &path, vector<vector<int>> &ans) {
+        if(root -> left == NULL and root -> right == NULL) {
+            if(total == target) {
+                ans.push_back(path);
+            }
             return;
         }
-        path.push_back(root->val);
-        if(root -> left != NULL) {
-            dfs(root -> left, targetSum, currSum + root -> val, path, ans);
+        if(root->left != NULL) {
+            path.push_back(root->left->val);
+            dfs(root->left,total + root->left->val,target, path,ans);
+            path.pop_back();
         }
-        if(root -> right != NULL) {
-            dfs(root -> right, targetSum, currSum + root -> val, path, ans);
+        if(root->right != NULL) {
+            path.push_back(root->right->val);
+            dfs(root->right,total + root->right->val,target, path,ans);
+            path.pop_back();
         }
-        path.pop_back();
     }
     
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if(root == NULL) {
+            return {};
+        }
         vector<int> path;
         vector<vector<int>> ans;
-        if(root == NULL)
-            return ans;
-        int currSum = 0;
-        dfs(root,targetSum,currSum,path,ans);
+        int total = root->val;
+        path.push_back(root->val);
+        dfs(root,total,targetSum,path,ans);
         return ans;
     }
 };
