c59b66c5dd6b1cae3dedb7883c834b55cfdf1be6
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-10-07 10:15:02 -0400

    Time: 22 ms (11.97%), Space: 14.2 MB (5.89%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        
         int n = nums.size();
-        int bestproduct = -11;
-        int maxi_indvalue = -11;
-        for(int i = 0; i < nums.size(); i++) {
-            int product = 1, negatives = 0, lastnegprod = 1, l_index = -1, firstnegprod = 1;
-            while(i < nums.size() and nums[i] != 0) {
-                maxi_indvalue = max(maxi_indvalue,nums[i]);
-                product *= nums[i];
-                lastnegprod *= nums[i];
-                if(nums[i] < 0) {
-                    l_index = i;
-                    negatives++;
-                    lastnegprod = nums[i];
-                }
-                if(firstnegprod > 0) {
-                    firstnegprod *= nums[i];
-                }
-                bestproduct = max(bestproduct,max(product,nums[i]));
-                i++;
-            }
-            if(i < nums.size()) {
-                bestproduct = max(bestproduct,0);
-            }
-            if(negatives % 2 == 0) {
-                if(negatives == 0)
-                    continue;
-                bestproduct = max(bestproduct,product);
+        vector<pair<int,int>> dp(n);
+        dp[0].first = nums[0];
+        dp[0].second = nums[0];
+        int maxi = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] < 0) {
+                dp[i].first = max(dp[i-1].second * nums[i],nums[i]);
+                dp[i].second = min(dp[i-1].first * nums[i],nums[i]);
             }
             else {
-                if(negatives == 1 and product == nums[l_index]) {
-                    continue;
-                }
-                bestproduct = max(bestproduct,max(product/firstnegprod,product/lastnegprod));
+                dp[i].first = max(dp[i-1].first * nums[i],nums[i]);
+                dp[i].second = min(dp[i-1].second * nums[i],nums[i]);
             }
+            maxi = max(maxi,dp[i].first);
+            maxi = max(maxi,dp[i].second);
         }
-        return bestproduct;
+        return maxi;
+        
     }
 };
